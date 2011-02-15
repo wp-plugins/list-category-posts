105c4fba532b7a5b50045856101e9edffad06249
fernandobt <fernandobt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-15 02:12:19 +0000

    Tagged version 0.18
    git-svn-id: https://plugins.svn.wordpress.org/list-category-posts/tags/0.18@345587 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/default.php
+++ templates/default.php
@@ -3,7 +3,7 @@
 Plugin Name: List Category Posts - Template
 Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
 Description: Template file for List Category Post Plugin for Wordpress which is used by plugin by argument template=value.php
-Version: 0.8
+Version: 0.9
 Author: Radek Uldrych & Fernando Briano 
 Author URI: http://picandocodigo.net http://radoviny.net
 */
@@ -31,9 +31,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * the instance of CatListDisplayer that called this file.
  */
 
-/* This is the string which will gather all the information.
- * We're starting it  */
+/* This is the string which will gather all the information.*/
 $lcp_display_output = '';
+
+// Show category link:
+$lcp_display_output .= $this->get_category_link($single, 'strong');
 
 //Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
 $lcp_output .= '<ul class="lcp_catlist">';
@@ -42,7 +44,12 @@ $lcp_output .= '<ul class="lcp_catlist">';
  * Posts loop.
  * The code here will be executed for every post in the category.
  * As you can see, the different options are being called from functions on the
- * $this variable which is a CatListDisplayer. The CatListDisplayer
+ * $this variable which is a CatListDisplayer.
+ *
+ * The CatListDisplayer has a function for each field we want to show.
+ * So you'll see get_excerpt, get_thumbnail, etc.
+ * You can now pass an html tag as a parameter. This tag will sorround the info
+ * you want to display. You can also assign a specific CSS class to each field.
  */
 foreach ($this->catlist->get_categories_posts() as $single):
     //Start a List Item for each post:
@@ -58,7 +65,7 @@ foreach ($this->catlist->get_categories_posts() as $single):
     $lcp_display_output .= ' ' . $this->get_date($single);
 
     //Show author
-    $lcp_display_output .= '<br/>' . __('Author') . ': ' . $this->get_author($single) . '<br/>';
+    $lcp_display_output .= $this->get_author($single);
 
     //Custom fields:
     $lcp_display_output .= $this->get_custom_fields($this->params['customfield_display'], $single->ID);
@@ -66,11 +73,17 @@ foreach ($this->catlist->get_categories_posts() as $single):
     //Post Thumbnail
     $lcp_display_output .= $this->get_thumbnail($single);
 
-    //Post content
-    $lcp_display_output .= $this->get_content($single);
+    /**
+     * Post content - Example of how to use tag and class parameters:
+     * This will produce:<p class="lcp_content">The content</p>
+     */
+    $lcp_display_output .= $this->get_content($single, 'p', 'lcp_content');
 
-    //Post excerpt
-    $lcp_display_output .= $this->get_excerpt($single);
+    /**
+     * Post content - Example of how to use tag and class parameters:
+     * This will produce:<div class="lcp_excerpt">The content</div>
+     */
+    $lcp_display_output .= $this->get_excerpt($single, 'div', 'lcp_expert');
 
     //Close li tag
     $lcp_display_output .= '</li>';
@@ -78,5 +91,3 @@ endforeach;
 
 $lcp_display_output .= '</ul>';
 $this->lcp_output = $lcp_display_output;
-
-?> 
