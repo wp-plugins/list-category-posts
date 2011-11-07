844709cd543d7963770ec1d1099486e1d8d3e9d6
fernandobt <fernandobt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-07 20:06:25 +0000

    Working on options page
    git-svn-id: https://plugins.svn.wordpress.org/list-category-posts/branches/lcp_options@460338 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/options.php
+++ include/options.php
@@ -17,6 +17,7 @@
   float: left;
 }
 
+
 </style>
 <div class="wrap">
 <div id="icon-tools" class="icon32"></div>
@@ -35,7 +36,7 @@
     </div>
   </div>
   
-  This options page generates the <a href="http://wordpress.org/extend/plugins/list-category-posts/">List Category Posts</a> shortcode for you to use in your post/page. Please select the options below, copy the generated code and pase it in your page.
+  <p>This options page generates the List Category Posts shortcode for you to use in your post/page. Please select the options below, copy the generated code and pase it in your page.</p>
   <form method="post" >
     <div class="lcp_form_row">
       <div class="lcp_label"><label for="categoryid"><?php _e("Category", 'list-category-posts')?></label></div>
@@ -82,28 +83,80 @@
     </div>
 
     <div class="lcp_form_row">
+      <div class="lcp_label"><label for="order"><?php _e("Order", 'list-category-posts')?></label></div>
+        <div class="lcp_form_elem"><select id="order" name="order" type="text">
+                <option value='desc'><?php _e("Descending", 'list-category-posts')?></option>
+                <option value='asc'><?php _e("Ascending", 'list-category-posts')?></option>
+          </select>
+        </div>
+    </div>
+
+    <div class="lcp_form_row">
       <div class="lcp_label"><label for="tag"><?php _e("Tags", 'list-category-posts')?></label></div>
       <div class="lcp_form_elem"><input size="24" id="tags" name="tags" type="text"
       value="<?php echo isset($_POST['tags']) ? $_POST['tags'] : ''; ?>" />
         </select></div>
     </div>
 
+    <div class="lcp_form_row">
+      <div class="lcp_label"><label for="exclude"><?php _e("Exclude categories (id's)", 'list-category-posts')?></label></div>
+        <div class="lcp_form_elem">
+          <input id="exclude" name="exclude" type="text"
+          value="<?php echo isset($_POST['exclude']) ? esc_attr($_POST['exclude']) : '';?>" />
+        </div>
+    </div>
     
-    <input class='button-primary' type='submit' name='getcode' value='<?php _e('Get code'); ?>' id='submitbutton' />
+    <div class="lcp_form_row">
+      <div class="lcp_label"><label for="excludeposts"><?php _e("Exclude posts (id's)", 'list-category-posts')?></label></div>
+        <div class="lcp_form_elem">
+          <input id="excludeposts" name="excludeposts" type="text" 
+          value="<?php echo isset($_POST['excludeposts']) ? esc_attr($_POST['excludeposts']) : '';?>" />
+        </div>
+    </div>
+
+    <div class="lcp_form_row">
+      <div class="lcp_label"><label><?php _e("Show", 'list-category-posts')?>: </label></div>
+        <div class="lcp_form_elem">
+            <input type="checkbox" <?php checked( (bool) isset($_POST['thumbnail']), true ); ?>
+            name="thumbnail" /> <?php _e("Thumbnail - size", 'list-category-posts')?>
+            <select id="thumbnail_size" name="thumbnail_size" type="text">
+                <option value='thumbnail'>thumbnail</option>
+                <option value='medium'>medium</option>
+                <option value='large'>large</option>
+                <option value='full'>full</option>
+            </select></br>
+            <input type="checkbox" <?php checked( (bool) isset($_POST['show_date']), true ); ?>
+            name="show_date" /> <?php _e("Date", 'list-category-posts')?></br>
+            <input type="checkbox" <?php checked( (bool) isset($_POST['show_author']), true ); ?>
+            name="show_author" /> <?php _e("Author", 'list-category-posts')?></br>
+            <input type="checkbox" <?php checked( (bool) $instance['show_catlink'], true ); ?>
+            name="show_catlink" /> <?php _e("Link to category", 'list-category-posts')?></br>
+              <input type="checkbox" <?php checked( (bool) $instance['show_excerpt'], true ); ?>
+            name="show_excerpt" /> <?php _e("Excerpt", 'list-category-posts')?></br>
+        </div>
+      </div>
+    
+    
+      <div style="clear: both"><input class='button-primary' type='submit' name='getcode' value='<?php _e('Get code'); ?>' id='submitbutton' /></div>
+    
   </form>
 
 <?php if (isset($_POST['categoryid'])){ ?>
-  
+
   <h3>Generated code</h3>
   <code>
-    <?php echo '[catlist id=' . $_POST['categoryid'];?>
-    <?php if (isset($_POST['numberposts']) && $_POST['numberposts'] != -1): echo ' numberposts=' . intval($_POST['numberposts']); endif; ?>
-    <?php if (isset($_POST['tags']) && $_POST['tags'] != ''): echo ' tags=' . preg_replace('/\s/', '', $_POST['tags']); endif; ?>
-    <?php echo ']';?>
+    <?php echo '[catlist id=' . $_POST['categoryid'];
+    if (isset($_POST['numberposts']) && $_POST['numberposts'] != -1): echo ' numberposts=' . intval($_POST['numberposts']); endif;
+    if (isset($_POST['offset']) && $_POST['offset'] != '0'): echo ' offset=' . intval($_POST['tags']); endif;
+    if (isset($_POST['orderby']) && $_POST['orderby'] != ''): echo ' orderby=' . $_POST['orderby']; endif;
+    if (isset($_POST['order']) && $_POST['order'] != ''): echo ' order=' . $_POST['order']; endif;
+    if (isset($_POST['tags']) && $_POST['tags'] != ''): echo ' tags=' . preg_replace('/\s/', '', $_POST['tags']); endif;
+    echo ']';?>
   </code>
 
   <p>Copy this code and paste it in the edit post/page textarea.</p>
 <?php } ?>
+
 
 <p><small>This page doesn't save any data to your database. It's just a helper page for you to generate valid List Category Posts shortcodes.</small></p>
 </div>
